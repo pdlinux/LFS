2665eb99700863ab39a80cc88baf48586e691975
ali asadi <ali1354.asadi@gmail.com>
2024-10-03 15:02:04 -0700

    update

--- 2/Preparing.sh
+++ 2/Preparing.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
-#2.6
+
+#2.5. Creating a File System on the Partition
+mkswap /dev/sda3
+mkfs -v -t ext4 /dev/sda2
+mkfs -v -t F32 /dev/sda1
+
+#2.6. Setting The $LFS Variable
 export LFS=/mnt/lfs
-#2.7
+
+#2.7. Mounting the New Partition
 mkdir -pv $LFS
 mount -v -t ext4 /dev/sda2 $LFS
 
